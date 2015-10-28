cb7d56990cf925dbef24ce7291896370d92ca150
Jean-Noël Lesdema <jean-noel.lesdema@cea.fr>
2015-10-28 17:20:24 +0100

    add FindWapiti.cmake

---
+++ cmake/FindWapiti.cmake
@@ -0,0 +1,16 @@
+# - Try to find wapiti
+# Once done this will define
+# wapiti_FOUND - system has wapiti
+# wapiti_INCLUDE_DIRS - include directories for wapiti
+# wapiti_LIBRARIES - libraries for wapiti
+FIND_PATH(wapiti_INCLUDE_DIRS wapiti.h
+PATHS $ENV{WAPITI_INCLUDE_DIR}/include)
+FIND_LIBRARY(wapiti_LIBRARIES NAMES wapiti
+PATHS $ENV{WAPITI_INCLUDE_DIR}/lib)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(wapiti DEFAULT_MSG
+wapiti_LIBRARIES
+wapiti_INCLUDE_DIRS)
+
+#set(wapiti_FOUND ${wapiti_FOUND})
+#MARK_AS_ADVANCED(wapiti_FOUND wapiti_INCLUDE_DIRS wapiti_LIBRARIES)
