336eab9cb56ae0e679c17142e23fc4b4b5aaaf3c
Samah Ghalloussi <samah.ghalloussi@cea.fr>
2015-11-12 19:03:48 +0100

    fix setenv content. suggest to source wapiti env.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -11,7 +11,7 @@ mkdir $WAPITI_INSTALL_DIR
 pushd $WAPITI_INSTALL_DIR
   echo 'DIR=$( cd `dirname $BASH_SOURCE[0]`  && pwd )'   >  $setenv_filename
   echo 'export WAPITI_INSTALL_DIR=$DIR'   >>  $setenv_filename
-  echo "export LD_LIBRARY_PATH=$WAPITI_INSTALL_DIR/lib:$LD_LIBRARY_PATH"   >>  $setenv_filename
+  echo 'export LD_LIBRARY_PATH=$WAPITI_INSTALL_DIR/lib:$LD_LIBRARY_PATH'   >>  $setenv_filename
 popd
 
 
@@ -20,3 +20,8 @@ cd build
   cmake ..
   make
   make install
+
+cd $WAPITI_INSTALL_DIR
+  echo -e "\e[31mYou may want to type the following command: \e[0m"
+  echo -e "\e[31m echo 'source $PWD/$setenv_filename' >> ~/.bashrc \e[0m"
+
